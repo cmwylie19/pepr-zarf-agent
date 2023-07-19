b0ecbd49355ab8054bf838ca6bb6d33e62900492
Case Wylie <cmwylie19@defenseunicorns.com>
2023-07-19 15:47:43 -0400

    [TASK] Pod Vertical
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- wasm-transform/main.go
+++ wasm-transform/main.go
@@ -16,6 +16,25 @@ import (
 	admission "k8s.io/api/admission/v1"
 )
 
+const AgentErrImageSwap = "Unable to swap the host for (%s)"
+const replaceOperation = "replace"
+
+type PatchOperation struct {
+	Op    string      `json:"op"`
+	Path  string      `json:"path"`
+	From  string      `json:"from,omitempty"`
+	Value interface{} `json:"value,omitempty"`
+}
+
+// ReplacePatchOperation returns a replace JSON patch operation.
+func ReplacePatchOperation(path string, value interface{}) PatchOperation {
+	return PatchOperation{
+		Op:    replaceOperation,
+		Path:  path,
+		Value: value,
+	}
+}
+
 // WASMRequest will be create in Pepr Admission Controller
 //
 //	type WASMRequest[T any] struct {
@@ -119,55 +138,122 @@ func ConvertAdmissionRequest(this js.Value, args []js.Value) interface{} {
 }
 
 func podTransform(this js.Value, args []js.Value) interface{} {
+	peprRequest := args[0].String()
+	imagePullSecretName := args[1].String()
+	targetHost := args[2].String()
+	srcReference := args[3].String()
 	pod := &corev1.Pod{}
+	var patchOperations []PatchOperation
 
-	// fmt.Println(args[0].String())
-	fmt.Println("Empty pod", pod.Name)
 	// Define a variable to hold the parsed JSON data
 	var data map[string]interface{}
 
 	// Unmarshal the JSON string into the data variable
-	err := json.Unmarshal([]byte(args[0].String()), &data)
+	err := json.Unmarshal([]byte(peprRequest), &data)
 	if err != nil {
 		log.Fatal(err)
 	}
 	// Convert the interface to a JSON byte array
-	jsonBytes, err := json.Marshal(data["Raw"])
+	podBytes, err := json.Marshal(data["Raw"])
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	err = json.Unmarshal(jsonBytes, pod)
+	// unmarshal podBytes into pod
+	err = json.Unmarshal(podBytes, pod)
 	if err != nil {
 		fmt.Println("Error unmarshalling pod", err)
-
 	}
 
-	// check if pod has ignore labels
-	// js.Value[0] is the pod,_input(AdmissionReview) from pepr
-
-	//targetHost := fmt.Sprintf("%s", args[1])
-	//srcReference := fmt.Sprintf("%s", args[2])
-	fmt.Printf("%s\n", pod.Name)
-	fmt.Println(fmt.Sprintf("%s", data["Raw"]))
-	return nil
-}
-func add(this js.Value, args []js.Value) interface{} {
-	if len(args) != 2 {
+	// don't do anything if pod has ignore labels
+	if checkIgnoreLabels(pod) {
+		fmt.Println("Pod has ignore labels, ignoring")
 		return nil
 	}
-	result := args[0].Int() + args[1].Int()
-	fmt.Println(result)
-	return result
+
+	patchOperations = addImagePullSecret(pod, imagePullSecretName, patchOperations)
+	patchOperations = transformContainerImages(pod, targetHost, srcReference, patchOperations)
+	patchOperations = addPatchedLabel(pod, patchOperations)
+
+	// fmt.Printf("%s\n", pod.Name)
+	fmt.Printf("%+v", pod)
+	// fmt.Println(fmt.Sprintf("%s", data["Raw"]))
+	// PrettyPrint
+	// Create an empty interface to unmarshal the JSON string
+	// Marshal the Pod object into a pretty printed JSON string
+	podBytes, err = json.MarshalIndent(pod, "", "  ")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return nil
+	}
+
+	// Convert the JSON bytes to a string
+	podString := string(podBytes)
+	return string(podString)
+}
+func addPatchedLabel(pod *corev1.Pod, patchOperations []PatchOperation) []PatchOperation {
+	pod.Labels["zarf-agent"] = "patched"
+	return append(patchOperations, ReplacePatchOperation("/metadata/labels/zarf-agent", "patched"))
+}
+func checkIgnoreLabels(pod *corev1.Pod) bool {
+	// check if pod has ignoreLables
+	if pod.Labels["zarf-agent"] == "patched" || pod.Labels["zarf.dev/agent"] == "ignore" {
+		// We've already played with this pod, just keep swimming 🐟
+		return true
+	}
+	return false
 }
 
+func addImagePullSecret(pod *corev1.Pod, imagePullSecretName string, patchOperations []PatchOperation) []PatchOperation {
+	pod.Spec.ImagePullSecrets = append(pod.Spec.ImagePullSecrets, corev1.LocalObjectReference{
+		Name: imagePullSecretName,
+	})
+	return append(patchOperations, ReplacePatchOperation("/spec/imagePullSecrets", imagePullSecretName))
+}
+
+func transformContainerImages(pod *corev1.Pod, targetHost, srcReference string, patchOperations []PatchOperation) []PatchOperation {
+	// update the image host for each init container
+	for idx, container := range pod.Spec.InitContainers {
+		path := fmt.Sprintf("/spec/initContainers/%d/image", idx)
+		replacement, err := transform.ImageTransformHost(targetHost, srcReference)
+
+		if err != nil {
+			fmt.Printf(AgentErrImageSwap, err)
+			continue // Continue, because we might as well attempt to mutate the other containers for this pod
+		}
+		container.Image = replacement
+		patchOperations = append(patchOperations, ReplacePatchOperation(path, replacement))
+	}
+
+	// update the image host for each ephemeral container
+	for idx, container := range pod.Spec.EphemeralContainers {
+		path := fmt.Sprintf("/spec/ephemeralContainers/%d/image", idx)
+		replacement, err := transform.ImageTransformHost(targetHost, srcReference)
+		if err != nil {
+			fmt.Printf(AgentErrImageSwap, err)
+			continue // Continue, because we might as well attempt to mutate the other containers for this pod
+		}
+		container.Image = replacement
+		patchOperations = append(patchOperations, ReplacePatchOperation(path, replacement))
+	}
+
+	// update the image host for each normal container
+	for idx, container := range pod.Spec.Containers {
+		path := fmt.Sprintf("/spec/containers/%d/image", idx)
+		replacement, err := transform.ImageTransformHost(targetHost, srcReference)
+		if err != nil {
+			fmt.Printf(AgentErrImageSwap, err)
+			continue // Continue, because we might as well attempt to mutate the other containers for this pod
+		}
+		container.Image = replacement
+		patchOperations = append(patchOperations, ReplacePatchOperation(path, replacement))
+	}
+	return patchOperations
+}
 func main() {
 
-	js.Global().Set("add", js.FuncOf(add))
-	js.Global().Set("ConvertAdmissionRequest", js.FuncOf(ConvertAdmissionRequest))
-	js.Global().Set("TransformImage", js.FuncOf(TransformImage))
-	js.Global().Set("GetResource", js.FuncOf(GetResource))
 	js.Global().Set("podTransform", js.FuncOf(podTransform))
+	js.Global().Set("TransformImage", js.FuncOf(TransformImage))
 	// research how to scope functions to a module
 	// - would  all wasm functions from an AdmissionPatch
 	// -- pod.Merge and return a deepPartial from WASM
